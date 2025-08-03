d35fe2ad3a075f8c40ed90cc30b94d729444f177
saleh-programs <muradsaleh2022@gmail.com>
2025-08-03 01:16:03 -0400

    Implemented basic sidebar resizing logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,27 @@ import styles from "styles/components/Sidebar.module.css"
 import { useRouter } from "next/navigation"
 
 import AccountHub from "src/components/AccountHub"
+import { useRef } from "react"
 
 function Sidebar(){
   const router = useRouter()
+  const sideBarRef = useRef(null)
+
+  function startDrag(){
+    function onMove(e){
+      sideBarRef.current.style.width = `${e.clientX}px`
+    }
+    function onRelease(){
+      document.removeEventListener("mousemove",onMove)
+      document.removeEventListener("mouseup",onRelease)
+    }
+    document.addEventListener("mousemove",onMove)
+    document.addEventListener("mouseup",onRelease)
+
+  }
   return(
-    <div className={styles.sidePanel}>
+    <div className={styles.sidePanel}  ref={sideBarRef}>
+      <section className={styles.sidePanelMain}>
         <section className={styles.features}>
           <button onClick={()=>router.push("/platform/chat")}>Chat</button>
           <button onClick={()=>router.push("/platform/documents")}>Documents</button>
@@ -15,8 +31,12 @@ function Sidebar(){
           <button onClick={()=>router.push("/platform/videochat")}>Video Chat</button>
         </section>
         <AccountHub/>
-      </div>  
+      </section>
+      <section className={styles.sidePanelHandle} onMouseDown={startDrag}>
+        hey
+      </section>
+    </div>  
       )
-}
+} 
 
 export default Sidebar
