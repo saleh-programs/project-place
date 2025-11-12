56a5c7496de8a362fc916f210d4ca7d7208a248e
saleh-programs <muradsaleh2022@gmail.com>
2025-11-11 22:22:47 -0500

    Styling to videochat page

--- src/app/platform/videochat/page.jsx
+++ src/app/platform/videochat/page.jsx
@@ -5,18 +5,20 @@ import styles from "styles/platform/VideoChat.module.css"
 import Animation from "src/components/Animation"
 import { useRouter } from "next/navigation"
 function VideoChat(){
-  const router = useRouter()
   const {darkMode} = useContext(ThemeContext)
+  const router = useRouter()
 
   return(
-    <div className={styles.videochatPage}>
+    <div className={`${styles.videochatPage} ${darkMode ? styles.darkMode : ""}`}>
       <h1 className={styles.title}>
         <Animation key={darkMode ? "dark" : "light"} path={darkMode ? "/dark/videochat?18" : "/light/videochat?18"} type="once" speed={4}/> 
       </h1>
-      <button onClick={()=>router.push(`/platform/videochat/peercall`)}>Call a Peer</button>
-      <button onClick={()=>router.push("/platform/videochat/groupcall")}>Join Group Call</button>
+      <div className={styles.mainContent}>
+        <button onClick={()=>router.push(`/platform/videochat/peercall`)}>Call a Peer</button>
+        <button onClick={()=>router.push("/platform/videochat/groupcall")}>Join Group Call</button>
+      </div>
     </div>
   )
 }
 
-export default VideoChat 
+export default VideoChat  
