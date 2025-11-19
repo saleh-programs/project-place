455dd9e3721bf4e9ee2bd43be247450ee36a9192
saleh-programs <muradsaleh2022@gmail.com>
2025-11-19 00:26:19 -0500

    Creating components to hold 2d balls that move around

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,23 @@
 //this will later need to be a nice page with descriptions everywhere
 
 import styles from "styles/Home.module.css"
+import BallContainer from "src/components/BallContainer"
 
 function Home(){
   return(
-  <div className={styles.homepage}>
+  <div >
     <button onClick={()=>{window.location.href="http://localhost:5000/login"}}>Log In</button>
     <button onClick={()=>{window.location.href="http://localhost:5000/logout"}}>Log Out</button>
+    <div style={{
+      width:"300px",
+      height: "300px", 
+      border: "1px solid red", 
+      position: "relative",
+      margin: "10px"
+      }}>
+        <BallContainer/>
+
+    </div>
   </div>
   )
 }
---
+++ src/components/BallContainer.jsx
@@ -0,0 +1,22 @@
+import styles from "styles/components/BallContainer.module.css"
+
+
+
+function Ball({src, origin}){
+    return (
+        <section style={{
+
+        }}>
+
+        </section>
+    )
+}
+function BallContainer({}){
+
+    return(
+        <div className={styles.ballContainer}>
+        </div>
+    )
+}
+
+export default BallContainer 
