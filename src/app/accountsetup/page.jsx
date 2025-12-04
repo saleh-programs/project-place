cc701e1aab1fcaf0c34dd1c06f4cddafdfc8643a
saleh-programs <muradsaleh2022@gmail.com>
2025-12-04 06:47:10 -0500

    Styling + constraints to username creation

--- src/app/accountsetup/page.jsx
+++ src/app/accountsetup/page.jsx
@@ -4,24 +4,61 @@ import { useEffect, useRef, useState } from "react";
 import {updateUserInfoReq } from "backend/requests";
 import { useRouter } from "next/navigation";
 
+import styles from "styles/accountsetup/AccountSetup.module.css"
 
 function AccountSetup(){
   const router = useRouter()
+
+  const [newUsername, setNewUsername] = useState("")
+  const [usernameSelected, setUsernameSelected] = useState(false)
+
+  const [errMessage, setErrMessage] = useState("")
+
   const inputRef = useRef(null)
 
   async function handleSubmit(){
-    const response = await updateUserInfoReq({"username": inputRef.current.value})
-    if (response){
-      router.push("/platform")
-    }
+    // const response = await updateUserInfoReq({"username": inputRef.current.value})
+    // if (response){
+    //   router.push("/platform")
+    // }
+
+    //ill verify username don't exist
+    setUsernameSelected(true)
+
+    //if it doesn't, show error
+    setErrMessage("That username already exists");
+
   }
+
+  function handleChange(e){
+    if (/^(?!\d)[a-zA-Z0-9_+!]*$/.test(e.target.value)){
+      setNewUsername(e.target.value)
+    } 
+  }
+
   return(
-    <div>
-      <input ref={inputRef} type="text" />
-      <button onClick={handleSubmit}>Submit</button>
-      account setup
+    <div className={styles.accountSetupPage}>
+      <div className={styles.interactable}>
+        {usernameSelected 
+        ? 
+          <section className={styles.pfpInput}>
+            
+
+          </section>
+        :
+          <section className={styles.usernameInput}>
+            <h2>Create your username</h2>
+            <input ref={inputRef} type="text" spellCheck="false"
+            value={newUsername}
+            onChange={handleChange}/>
+            <span>{errMessage}</span>
+            <button onClick={handleSubmit}>Submit</button>
+          </section>
+        }
+
+      </div>
     </div>
   )
 }
-
-export default AccountSetup;
+ 
+export default AccountSetup; 
